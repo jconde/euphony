5ad470b919a49010711f7426598b35d9396bcb49
Javier Conde <jcondem@gmail.com>
2015-11-03 11:04:30 -0600

    Code added

---
+++ WebRTC.js
@@ -0,0 +1,290 @@
+// Node WebRTC
+
+var https = require("https"); // Tiene que ser HTTPS porque Chrome no comparte la pantalla por HTTP
+var url = require("url");
+var fs = require("fs");
+var util = require("util");
+
+var httpsport = 443;
+
+// HTTPS Web Server
+
+var httpsoptions = {
+	key: fs.readFileSync('server.key'),
+	cert: fs.readFileSync('server.crt')
+};
+
+var webServer = https.createServer(httpsoptions, function (request, response) {
+
+    var req = url.parse(request.url, true);
+	var path = req.path; console.log(JSON.stringify(req));
+
+	if (path == '/') {
+		console.log('Index accessed.');
+		fs.readFile('./index.html', function(err, html) {
+			if (err) {
+				response.writeHead(404);
+				response.write('404 - Page not found.');
+				response.end();
+			}
+			else {
+				response.writeHead(200);
+				response.write(html);
+				response.end();
+			}
+		});
+		
+	} else if (path == '/client.js') {
+		console.log('Client accesed.');
+		fs.readFile('./client.js', function (err, js) {
+			if (err) {
+				response.writeHead(404);
+				response.write('404 - Page not found.');
+				response.end();
+			}
+			else {
+				response.writeHead(200, {"Content-Type": 'text/javascript'});
+				response.write(js);
+				response.end();
+			}
+		});
+		
+	} else if (path == '/style.css') {
+		fs.readFile('./style.css', function (err, css) {
+			if (err) {
+				response.writeHead(404);
+				response.write('404 - Page not found.');
+				response.end();
+			}
+			else {
+				response.writeHead(200, {"Content-Type": 'text/javascript'});
+				response.write(css);
+				response.end();
+			}
+		});
+		
+	} else if (path == '/bg.jpg') {
+		fs.readFile('./bg.jpg', function (err, jpg) {
+			if (err) {
+				response.writeHead(404);
+				response.write('404 - Page not found.');
+				response.end();
+			}
+			else {
+				response.writeHead(200);
+				response.write(jpg);
+				response.end();
+			}
+		});
+		
+	} else if (path == '/bg2.jpg') {
+		fs.readFile('./bg2.jpg', function (err, jpg) {
+			if (err) {
+				response.writeHead(404);
+				response.write('404 - Page not found.');
+				response.end();
+			}
+			else {
+				response.writeHead(200);
+				response.write(jpg);
+				response.end();
+			}
+		});
+		
+	} else if (path == '/favicon.ico') {
+		fs.readFile('./favicon.ico', function (err, ico) {
+			if (err) {
+				response.writeHead(404);
+				response.write('404 - Page not found.');
+				response.end();
+			}
+			else {
+				response.writeHead(200);
+				response.write(ico);
+				response.end();
+			}
+		});
+		
+	} else if (path == '/polloallimonmolamogollon') {
+		console.log('Restarting the node.');
+		process.exit(0);
+		
+	} else {
+		console.log('Room accesed: ' + path);
+		fs.readFile('./client.html', function(err, html) {
+			if (err) {
+				response.writeHead(404);
+				response.write('404 - Page not found.');
+				response.end();
+			}
+			else {
+				response.writeHead(200);
+				response.write(html);
+				response.end();
+			}
+		});
+	}
+}).listen(httpsport);
+
+console.log("Web Server is listening in port " + httpsport);
+
+
+// WebSockets Server
+
+var rooms = {};
+
+var io = require("socket.io")(webServer);
+
+io.sockets.on('connection', function (socket) {
+
+	var address = socket.handshake.address.address;
+	console.log((new Date()) + ' Peer connected: ' + address);
+	
+	socket.on('login', function(user, room) {
+		
+		// Check illegal character '#'
+		if ((user.indexOf('#') >= 0) || (room.indexOf('#') >= 0)) {
+			console.log('User or room error: illegal character \'#\'.');
+			socket.disconnect();
+			return;
+		}
+		
+		if (rooms[room] === undefined) {
+			rooms[room] = {'userlist' : {}, 'mod' : [user], 'ban' : [], 'mute' : [] };
+			socket.emit('admin', user, 'mod');
+		}
+		else if (rooms[room].userlist[user] !== undefined) {
+			console.log('User already exists in the room.')
+			socket.disconnect();
+			return;
+		}
+		else if (rooms[room].ban.indexOf(address) >= 0) {
+			console.log(address + ' banned in room ' + room + '.');
+			socket.disconnect();
+			return;
+		}
+		
+		socket.room = room;
+		socket.user = user;
+		
+		socket.emit('userlist', Object.keys(rooms[room].userlist));
+		bcast(socket, 'hello', '');
+		rooms[room].userlist[user] = socket;
+
+		socket.on('message', function(data) {
+			bcast(this, 'message', data);
+		});
+		
+		// WebRTC functions
+		socket.on('offer', function (to, data) {
+			send(this, 'offer', to, data);
+		});
+	
+		socket.on('answer', function (to, data) {
+			send(this, 'answer', to, data);
+		});
+	
+		socket.on('ice', function (to, data) {
+			send(this, 'ice', to, data);
+		});
+		
+		// WebRTC stream routing request
+		socket.on('route', function(to,data) {
+			send(this, 'route', to, data);
+		});
+
+		// Moderation
+		socket.on('admin', function(to, data) {
+		
+			var room = socket.room;
+			var from = socket.user;
+			
+			if (rooms[room].userlist[to] === undefined) return;
+			
+			var mod = rooms[room].mod.indexOf(from) >= 0;
+			var muted = rooms[room].mute.indexOf(to) >= 0;
+			var address = rooms[room].userlist[to].handshake.address.address;
+			
+			switch (data) {
+				case 'mod': if (mod && (rooms[room].mod.indexOf(to) < 0)) {
+								rooms[room].mod.push(to);
+								bcast_admin(socket, to, 'mod');
+							}
+							break;
+				case 'ban': if (mod && (rooms[room].ban.indexOf(address) < 0)) {
+								rooms[room].ban.push(address);
+							}
+				case 'kick': if (mod) {
+								 rooms[room].userlist[to].emit('admin', to, 'kicked');
+								 rooms[room].userlist[to].disconnect();
+							 }
+							 break;
+				case 'unban': if (mod && (rooms[room].ban.indexOf(address) >= 0)) {
+								 rooms[room].ban.splice(rooms[room].ban.indexOf(address),1);
+							  }
+							  break;
+				case 'mute': if (mod && !muted) {
+								 rooms[room].mute.push(to);
+								 bcast_admin(socket, to, 'mute');
+							 }
+							 else if (from == to) {
+								 bcast_admin(socket, to, 'mute');
+							 }
+							 break;
+				case 'unmute': if (!unmuted && (from == to)) {
+								   bcast_admin(socket, to, 'unmute');
+							   }
+							   else if (mod && muted) {
+								   rooms[room].mute.splice(rooms[room].mute.indexOf(to), 1);
+								   bcast_admin(socket, to, 'unmute');
+							   }
+							   break;
+				default: break;
+			}
+		});
+		
+		socket.on('disconnect', function () {
+		
+			var room = socket.room;
+			var user = socket.user;
+			bcast(socket, 'bye', '');
+			delete rooms[room].userlist[user];
+			if (Object.keys(rooms[room].userlist).length == 0) {
+				delete rooms[room];
+			}
+			else {
+				var mod = rooms[room].mod.indexOf(user);
+				if (mod > -1) rooms[room].mod.splice(mod);
+			
+				var muted = rooms[room].mute.indexOf(user);
+				if (mod > -1) rooms[room].mute.splice(muted);
+			}
+		});
+	});
+});
+
+// Broadcast a message
+function bcast(socket, tipo, msg) {
+	var room = socket.room;
+	var from = socket.user;
+	for (var to in rooms[room].userlist) {
+		rooms[room].userlist[to].emit(tipo, from, msg);
+	}
+
+};
+
+// Send a message
+function send(socket, tipo, to, msg) {
+	var room = socket.room;
+	var from = socket.user;
+	if (rooms[room].userlist[to] !== undefined) rooms[room].userlist[to].emit(tipo, from, msg);
+};
+
+// Admin broadcasts
+function bcast_admin(socket, to, command) {
+	var room = socket.room;
+	var from = socket.user;
+	for (var user in rooms[room].userlist) {
+		rooms[room].userlist[user].emit('admin', to, command);
+	}
+};
---
+++ client.js
@@ -0,0 +1,352 @@
+"use strict"
+
+///////
+// Room
+
+var room = angular.module('Room', []);
+
+// Config
+
+room.config(function($sceDelegateProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist([
+   'self',
+   "http**",
+	 "blob**"
+  ]);
+});
+
+// Controller
+
+room.controller('Ctrl',['$scope','Room',function ($scope,Room) {
+  
+  $scope.$on('localVideo.update', function (e) {
+		if (Room.localStream)
+			$scope.localVideo = webkitURL.createObjectURL(Room.localStream);
+		else
+			$scope.localVideo = null;
+		if ($scope.status.streamType == 'camera')
+			$scope.localVideoTransform = '-webkit-transform : scaleX(-1)';
+		else
+			$scope.localVideoTransform = '';
+		$scope.$apply();
+  });
+	
+	$scope.$on('userVideo.update', function (e,user) {
+		console.log(Room.users[user].streams.length);
+		$scope.$apply($scope.users[user].video = Room.users[user].streams.map(function(stream) { return webkitURL.createObjectURL(stream) }));
+	});
+	
+	$scope.$on('userlist.update', function (e) {
+		$scope.$apply($scope.users = Room.users);
+	});
+	
+	$scope.$on('messageLog.update', function (e) {
+		$scope.$apply($scope.messageLog = Room.messageLog);
+	});
+  
+	Room.init();
+	
+	$scope.username = Room.username;
+	$scope.roomname = Room.roomname;
+  $scope.users = Room.users;
+	$scope.status = Room.status;
+	$scope.messageLog = Room.messageLog;
+	
+	setInterval(function(){
+			if (document.URL != Room.url)
+				location.reload();
+		}, 
+	1000);
+	
+}]);
+
+
+// Directives
+
+room.directive("chat", ['Room', function (Room) {
+	return {
+		link: function(scope,element,attrs) {
+			element.bind("keypress", function(e) {
+				if (e.which === 13) {
+					Room.sendMsg(scope.msg);
+					scope.$apply(scope.msg = '');
+				}
+			});
+		}
+	};
+}]);
+
+room.directive("button", ['Room', function (Room) {
+  return {
+    restrict: "A",
+    link: function(scope,element,attr) {
+      element.bind("click", function() {
+        console.log(element[0].innerText);
+        if (element[0].innerText.match('Add'))
+          Room.userAdd(scope.uid,scope.pc);
+        else if (element[0].innerText.match('Del'))
+          Room.userDel(scope.uid);
+        else
+          Room.call(scope.uid);
+      });
+    }
+  };
+}]);
+
+
+
+////////////
+// Service
+
+
+room.service('Room', ['$rootScope', function($rootScope) {
+  var room = {
+    
+		cons : {
+			camera : {'video': true, 'audio': true},
+			audioOnly : {'video': false, 'audio': true},
+			screen : {'video': {mandatory: {chromeMediaSource: 'screen'}}, 'audio': false}
+		},
+
+		status : {
+			connected : false,
+			muted : false,
+			mod : false,
+			streamType : 'camera',
+		},
+		
+		roomname : "",
+		username : "",
+		localStream : null,
+    users : {},
+		socket : "",
+		messageLog : [],
+		
+		init : function() {
+			if (document.URL.match(/#/g).length !== 1) {
+				var name = prompt("Please enter your user name","");
+				window.location.href = document.URL + '#' + name;	// Needs to be changed
+			}
+			this.url = document.URL;
+			this.username = document.URL.split('#').pop() || '';
+			this.roomname = document.URL.split('/').pop().split('#')[0];
+			var url = document.URL.split(this.roomname)[0];
+			
+			// if (!this.status.connected) {
+				this.socket = io(url);
+				this.status.connected = true;
+			// } else {
+				// this.socket.socket.reconnect();
+				// this.socket.removeAllListeners();
+			// }
+			
+			// Socket events init
+			
+			this.socket.on('connect', function() {
+				this.emit('login', room.username, room.roomname);
+				console.log('Room: ' + room.roomname + ', User: ' + room.username);
+			});
+			
+			this.socket.on('userlist', function(list) {
+				list.map(function(x) { room.users[x] = { 'pc' : '', 'streams' : [], 'dc' : {}, 'stats' : {}, 'status' : { 'muted' : false } }; });
+				room.initStream(room.cons.camera);
+				// $rootScope.$broadcast('userlist.update');
+			});
+			
+			this.socket.on('hello', function(from, data) {
+				room.userAdd(from);
+			});
+
+			this.socket.on('bye', function(from, data) {
+				room.userDel(from);
+			});
+			
+			this.socket.on('message', function(from, data) {
+				if (from !== room.username) {
+					var d = new Date();
+					var minutes = d.getMinutes() > 9 ? d.getMinutes() : '0' + d.getMinutes();
+					room.messageLog.push({ 'user' : from, 'msg' : data, 'time' : d.getHours() + ':' + d.getMinutes() });
+					$rootScope.$broadcast('messageLog.update');
+				}
+			});
+			
+			this.socket.on('offer', function(from, data) {
+				console.log('Llamada recibida: ' + JSON.stringify(data));
+				if (!room.status.muted)
+					room.users[from].pc.addStream(room.localStream);
+				room.users[from].pc.setRemoteDescription(new RTCSessionDescription(data));
+				room.users[from].pc.createAnswer(function(answer) {
+					room.users[from].pc.setLocalDescription(answer);
+					room.socket.emit('answer', from, answer);
+				}, null, {});
+			});
+			
+			this.socket.on('answer', function(from, data) {
+				console.log('Respuesta recibida: ' + JSON.stringify(data));
+				room.users[from].pc.setRemoteDescription(new RTCSessionDescription(data));
+			});
+			
+			this.socket.on('ice', function(from, data) {
+				console.log('Candidato ICE recibido: ' + JSON.stringify(data));
+				if (data)
+					room.users[from].pc.addIceCandidate(new RTCIceCandidate(data));
+			});
+			
+			this.socket.on('admin', function(user, data) {
+				console.log('Status actualizado: ' + JSON.stringify(data));
+				if (room.username === user)
+					switch (data) {
+						case 'mod': room.status.mod = true;
+												break;
+						case 'mute': room.status.muted = true;
+												 for (var user in room.users)
+													 room.users[user].pc.removeStream(room.localStream);
+												 room.localStream.stop();
+												 break;
+						case 'unmute': room.status.muted = false;
+													 room.initStream(room.cons.camera);
+													 break;
+					}
+				else
+					switch(data) {
+						case 'mute': room.users[user].status.muted = true;
+												 break;
+						case 'unmute': room.users[user].status.muted = false;
+													 break;
+					}
+			});
+			
+			this.socket.on('disconnect', function() {
+				for (var user in room.users)
+					room.userDel(user);
+				if (room.localStream)
+					room.localStream.stop();
+			});
+		},
+    
+		userAdd : function(user) {
+			if (user) {
+				if (!this.users[user])
+					this.users[user] = { 'pc' : '', 'streams' : [], 'dc' : {}, 'stats' : {}, 'status' : { 'muted' : false } };
+				this.users[user].pc = new webkitRTCPeerConnection({iceServers:[{url:"stun:stun.l.google.com:19302"}]}, {optional: [{RtpDataChannels: true}]});
+				this.users[user].pc.onconnecting = function(message) { console.log("Connecting.."); };
+				this.users[user].pc.onopen = function(message) { console.log("Call established."); };
+				this.users[user].pc.onaddstream = function (event) {
+					console.log("Stream coming from the other side.");
+					room.users[user].streams.push(event.stream);
+					$rootScope.$broadcast('userVideo.update',user);
+					room.users[user].stats.catcher = setInterval(room.getBitrate, 5000, user);
+				};
+				this.users[user].pc.onremovestream = function (event) {
+					console.log("Stream removed from the other side");
+					room.users[user].streams.splice(room.users[user].streams.indexOf(event.stream),1);
+					$rootScope.$broadcast('userVideo.update',user);
+					clearInterval(room.users[user].stats.catcher);
+					room.users[user].stats = {};
+				};
+				this.users[user].pc.onicecandidate = function (event) { room.socket.emit('ice', user, event.candidate); };
+				if (!this.status.muted)
+					this.users[user].pc.addStream(this.localStream);
+				this.users[user].pc.ondatachannel = function (event) { if (room.users[user].dc === {}) room.initDC(user, event.channel); };
+				$rootScope.$broadcast('userlist.update');
+			}
+		},
+  
+    userDel : function(user) {
+			if (this.users[user]) {
+				clearInterval(this.users[user].stats.catcher);
+				delete this.users[user];
+			}
+			$rootScope.$broadcast('userlist.update');
+    },
+    
+    call : function(user) {
+      if (typeof(this.users[user]) !== 'undefined') {
+        if (this.users[user].dc === '')
+					this.initDC(user, this.users[user].pc.createDataChannel('data', { reliable: true }));
+				this.users[user].pc.createOffer(function(offer) {
+					room.users[user].pc.setLocalDescription(offer);
+					room.socket.emit('offer', user, offer);
+				}, null, {});
+			}
+    },
+		
+		sendMsg : function(msg) {
+			var d = new Date();
+			var minutes = d.getMinutes() > 9 ? d.getMinutes() : '0' + d.getMinutes();
+			this.messageLog.push({ 'user' : this.username, 'msg' : msg, 'time' : d.getHours() + ':' + minutes });
+			this.socket.emit('message',msg);
+			$rootScope.$broadcast('messageLog.update');
+		},
+		
+		switchStream : function(type) {
+			this.status.streamType = type;
+			if (this.status.streamType !== type)
+				this.initStream(this.cons[type]);
+		},
+		
+		initStream : function(cons) {
+			navigator.webkitGetUserMedia(cons, this.onMediaSuccess, this.onMediaError);
+		},
+		
+		onMediaSuccess : function(stream) {
+			var oldStream = room.localStream;
+			room.localStream = stream;
+			for (var user in room.users) {
+				if (room.users[user].pc === '')
+					room.userAdd(user);
+				if (oldStream)
+					room.users[user].pc.removeStream(oldStream);
+				if (!room.status.muted)
+					room.users[user].pc.addStream(room.localStream);
+				room.call(user);
+			}
+			if (oldStream)
+				oldStream.stop();
+			$rootScope.$broadcast('localVideo.update');
+		},
+		
+		onMediaError : function(error) {
+			alert("Error on getUserMedia: " + error);
+		},
+		
+		initDC : function(user, channel) {
+			this.users[user].dc = {};
+			this.users[user].dc.buffer = [];
+			this.users[user].dc.sending = false;
+
+			channel.onopen = function() { console.log('Channel created with ' + user); };
+			channel.onclose = function() { console.log('Channel closed with ' + user); };
+			channel.onerror = function(err) { console.log('Channel error: ' + err); };
+			// channel.onmessage = function(event) { this.receiveFile(event.data, user); };
+			this.users[user].dc.channel = channel;
+		},
+		
+		admin : function(op,user) {
+			this.socket.emit('admin','user',op);
+		},
+		
+		getBitrate : function(user) {
+			room.users[user].pc.getStats(function f(stats) {
+				var results = stats.result();
+				results.map(function(res) {
+					if (res.type == 'ssrc' && res.stat('googFrameHeightReceived')) {
+						var bytesNow = res.stat('bytesReceived');
+						if (room.users[user].stats.timestampPrev) {
+							var bitRate = Math.round((bytesNow - room.users[user].stats.bytesPrev) * 8 / (res.timestamp - room.users[user].stats.timestampPrev));
+							console.log(user + ': ' + bitRate + ' kbits/sec');
+							room.users[user].stats.bitRate = bitRate;
+						}
+						room.users[user].stats.bytesPrev = bytesNow;
+						room.users[user].stats.timestampPrev = res.timestamp;
+					}
+				});
+			});
+		}
+  };
+  
+  return room;
+  
+}]);
+
+
